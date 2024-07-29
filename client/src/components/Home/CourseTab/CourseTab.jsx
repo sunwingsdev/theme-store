dc9be1be938d4afd1f7d313e1e9c7e866d09eb05
vismodeb <vismodeb2000@gmail.com>
2024-07-29 14:42:05 +0600

    website deatails page created

--- client/src/components/Home/CourseTab/CourseTab.jsx
+++ client/src/components/Home/CourseTab/CourseTab.jsx
@@ -2,95 +2,19 @@ import { Tab } from "@headlessui/react";
 import CourseCard from "../PopularCourse/CourseCard";
 import { useGetAllCourseQuery } from "../../../redux/features/allApis/coursesApi/coursesApi";
 
-// function classNames(...classes) {
-//   return classes.filter(Boolean).join(" ");
-// }
-
 export default function CourseTab() {
   const { data } = useGetAllCourseQuery();
 
-  // const tabList = [
-  //   { name: "all-course", label: "All Course" },
-  //   { name: "graphic-multimedia", label: "Graphic & Multimedia" },
-  //   { name: "web-software", label: "Web & Software" },
-  //   { name: "digital-marketing", label: "Digital & Marketing" },
-  // ];
-
   return (
-    <div className="w-full px-2 py-16 sm:px-0">
+    <div className="w-full px-2 sm:px-0">
       <Tab.Group>
-        <Tab.List className="flex rounded-xl p-1 text-black flex-row items-center justify-center gap-3">
-          {/* {tabList &&
-            tabList?.map((tab, i) => (
-              <Tab
-                // onClick={() => handleSelect(tab.category)} // Make sure to pass the category to handleSelect
-                className={`px-6 py-1 font-semibold capitalize border-b-2 border-transparent hover:border-red-600 hover:text-red-600`}
-                key={i} // Use tab._id as key
-              >
-                {tab.label}
-              </Tab>
-            ))} */}
-          {/* {Object.keys(categories).map((category) => (
-            <Tab
-              key={category}
-              className={({ selected }) =>
-                classNames(
-                  "w-full rounded-lg py-2.5 text-sm font-medium leading-5",
-                  "ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-                  selected
-                    ? "bg-white text-blue-700 shadow"
-                    : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
-                )
-              }
-            >
-              {category}
-            </Tab>
-          ))} */}
-        </Tab.List>
+        <Tab.List className="flex rounded-xl p-1 text-black flex-row items-center justify-center gap-3"></Tab.List>
         {
           <Tab.Panels className="mt-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 container mx-auto gap-10 md:gap-6 py-10">
             {data &&
               data?.map((course) => (
                 <CourseCard key={course?._id} course={course} />
               ))}
-            {/* {Object.values(categories).map((posts, idx) => (
-            <Tab.Panel
-              key={idx}
-              className={classNames(
-                "rounded-xl bg-white p-3",
-                "ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
-              )}
-            >
-              <ul>
-                {posts.map((post) => (
-                  <li
-                    key={post.id}
-                    className="relative rounded-md p-3 hover:bg-gray-100"
-                  >
-                    <h3 className="text-sm font-medium leading-5">
-                      {post.title}
-                    </h3>
-
-                    <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                      <li>{post.date}</li>
-                      <li>&middot;</li>
-                      <li>{post.commentCount} comments</li>
-                      <li>&middot;</li>
-                      <li>{post.shareCount} shares</li>
-                    </ul>
-
-                    <a
-                      href="#"
-                      className={classNames(
-                        "absolute inset-0 rounded-md",
-                        "ring-blue-400 focus:z-10 focus:outline-none focus:ring-2"
-                      )}
-                    />
-                  </li>
-                ))}
-              </ul>
-            </Tab.Panel>
-          ))} */}
           </Tab.Panels>
         }
       </Tab.Group>
---
+++ client/src/pages/Home/websiteDetails/WebsiteDetails.jsx
@@ -0,0 +1,150 @@
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
+const WebsiteDetails = () => {
+  return (
+    <div className="">
+      {/* single website details page header area */}
+      <div className="bg-slate-900 text-center text-white py-14 lg:py-16 xl:py-20 text-2xl lg:text-4xl font-bold flex flex-col items-center justify-center">
+        <h2>ESchool</h2>
+        <img
+          className="w-20 mx-auto"
+          src="https://themesbazar.com/wp-content/themes/themesbazar/assets/images/icon-image.png"
+          alt=""
+        />
+      </div>
+      {/* all single card */}
+      <div className="container mx-auto">
+        <div className="flex items-center justify-center mt-10 md:mt-16">
+          <Link className="bg-white hover:bg-red-600 hover:text-white text-red-600 border border-red-600 rounded-full p-2 px-10 text-lg font-bold hover:shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] transition-all duration-300">
+            Live Demo
+          </Link>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-10 md:py-14 lg:py-20 px-6 lg:px-10">
+          {/* single card */}
+          <div className="py-10 px-6 text-center bg-slate-100 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] transition-all duration-300 rounded">
+            <div className="space-y-3">
+              <h2 className="text-xl md:text-2xl font-bold border-b-2 py-1 mb-6">
+                Single License
+              </h2>
+              <h3 className="text-2xl md:text-3xl font-bold text-[#3c3b3e]">
+                $42.38 / 4,000 ৳
+              </h3>
+              <p className="text-base">
+                <span className="font-bold">One </span>Premium themes
+              </p>
+              <p>1 Year Updates</p>
+              <p>One Domain Usage</p>
+            </div>
+            <button className="bg-slate-500 hover:bg-blue-700 text-white rounded-full w-[70%] p-2 text-lg font-bold mt-10 hover:shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]  transition-all duration-300">
+              Buy Now
+            </button>
+          </div>
+          {/* single card */}
+          <div className="py-10 px-6 text-center bg-slate-100 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] transition-all duration-300 rounded">
+            <div className="space-y-3">
+              <h2 className="text-xl md:text-2xl font-bold border-b-2 py-1 mb-6">
+                Unlimited License
+              </h2>
+              <h3 className="text-2xl md:text-3xl font-bold text-[#3c3b3e]">
+                ৳10,000 / $106
+              </h3>
+              <p className="text-base">
+                <span className="font-bold">One </span>Premium themes
+              </p>
+              <p>1 Year Updates</p>
+              <p>Unlimited Domain Usage</p>
+            </div>
+            <button className="bg-slate-500 hover:bg-blue-700 text-white rounded-full w-[70%] p-2 text-lg font-bold mt-10 hover:shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]  transition-all duration-300">
+              Buy Now
+            </button>
+          </div>
+          {/* single card */}
+          <div className="py-10 px-6 bg-gray-300 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded">
+            <div className="space-y-3">
+              <h2 className="text-xl md:text-2xl font-bold border-b-2 py-1 mb-4">
+                Responsive
+              </h2>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-bold">
+                  <IoCheckmarkCircleOutline />{" "}
+                </span>
+                Responsive Design
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-bold">
+                  <IoCheckmarkCircleOutline />{" "}
+                </span>
+                Responsive Navigation
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-bold">
+                  <IoCheckmarkCircleOutline />{" "}
+                </span>
+                Responsive Editing
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-bold">
+                  <IoCheckmarkCircleOutline />{" "}
+                </span>
+                Excellent layout
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-bold">
+                  <IoCheckmarkCircleOutline />{" "}
+                </span>
+                Optimized for Faster Loading
+              </p>
+            </div>
+          </div>
+          {/* single card */}
+          <div className="order-5 md:col-span-2 bg-slate-100 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] mt-5 rounded">
+            <img
+              className="w-full"
+              src="https://www.thecreativemomentum.com/hs-fs/hubfs/blog-files/2106%20blogs/2106-21-Marketing101WebHomepage-image-h2-1.jpg?width=600&name=2106-21-Marketing101WebHomepage-image-h2-1.jpg"
+              alt=""
+            />
+          </div>
+          {/* single card */}
+          <div className="order-4 lg:order-6 py-10 px-6 bg-gray-300 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded">
+            <div className="space-y-3">
+              <h2 className="text-xl md:text-2xl font-bold border-b-2 py-1 mb-4">
+                Website Details
+              </h2>
+              <p className="text-base flex gap-1 border-b">
+                <span className="font-medium">Latest updated :</span>
+                25 July 2023
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-medium">Created :</span>
+                25 July 2023
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-medium">Documentation :</span>
+                Well Documented
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-medium">Layout :</span>
+                Responsive
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-medium">Widget Ready :</span>
+                Yes
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-medium">Compatible Browsers :</span>
+                IE11, Firefox, Safari, Opera, Chrome, Edge
+              </p>
+              <p className="text-base flex items-center gap-1 border-b">
+                <span className="font-medium">Software Version :</span>
+                React 6.4.1, React 6.4.2, React 6.4.3 ++
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WebsiteDetails;
